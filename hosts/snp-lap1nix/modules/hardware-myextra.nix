9cd2f83926d58b80705d0725be1f7e6deb1a7b90
snuppy <snuppy.code+git@pm.me>
2026-07-14 15:03:15 +0200

    work on be200 wifi resume fix
    seems like matching on subsystems might not be such a good idea,,
    https://admin.pci-ids.ucw.cz/mods/?action=jump?origin=help it seems to
    have like,, 4 different registered ones? but it's the same actual chip nya
    thinks so it's probably the same issue across all of them

--- hosts/snp-lap1nix/modules/hardware-myextra.nix
+++ hosts/snp-lap1nix/modules/hardware-myextra.nix
@@ -30,8 +30,8 @@
 
   # Wifi doesn't work after returning from sleep on nya's `Lenovo Yoga Pro 7 14IAH10`, more specifically it seems like nya has: `Intel(R) Wi-Fi 7 BE200 320MHz [Gale Peak]`
   # https://community.intel.com/t5/Wireless/Intel-WiFi-7-BE200-loses-connection-after-suspend-resume-on/m-p/1693403/highlight/true
-  # Nya thinks the problem is with d3cold, so this just matches for some of the wifi card's attributes that nya found with udevadm and lspci, and sets d3cold_allowed to "0"
+  # Nya thinks the problem is with d3cold, so this just matches vendor and device (which should be unique to this chip and its variants?) that nya found with udevadm and lspci, and sets d3cold_allowed to "0"
   services.udev.extraRules = ''
-    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{subsystem_vendor}=="0x8086", ATTR{subsystem_device}=="0x00f4", ATTR{device}=="0x272b", ATTR{d3cold_allowed}="0"
+    ACTION=="add", SUBSYSTEM=="pci", ATTR{vendor}=="0x8086", ATTR{device}=="0x272b", ATTR{d3cold_allowed}="0"
   '';
 }
