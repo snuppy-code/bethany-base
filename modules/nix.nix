0f1e4bf533459ae67330bfd02d42770b148e1e25
snuppy <snuppy.code+git@pm.me>
2026-07-15 23:24:36 +0200

    nevermind not doing that right now

--- modules/nix.nix
+++ modules/nix.nix
@@ -20,17 +20,6 @@
     "A /etc/nixos/terminal-dogma - - - - g:nerv-staff:rwx,d:g:nerv-staff:rwx,o::r" # x on ALL FILES is not great but nya can't see a way to do it without `setfacl` so the alternative is prob a systemd oneshot
   ];
 
-  systemd.user.services.setPermsOn = {
-    enable = true;
-    after = ["network.target"];
-    wantedBy = ["default.target"];
-    description = "My Cool User Service";
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''/my/cool/user/service'';
-    };
-  };
-
   programs.nh = {
     enable = true;
     clean.enable = true;
