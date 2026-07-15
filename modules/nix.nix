d58dada380f27f984113f00fae0f3946548c404a
snuppy <snuppy.code+git@pm.me>
2026-07-15 16:29:21 +0200

    fix ssh perms (see note)
    this is only half of the puzzle! nya also looked up what permissions
    .ssh wants and set those manually. basically if we set recursive ACLs we
    need to exclude the ssh stow area cuz openssh gets pissy (rightly so),
    but nya doesnt needd that for bethany base

--- modules/nix.nix
+++ modules/nix.nix
@@ -14,9 +14,22 @@
   };
 
   systemd.tmpfiles.rules = [
-    "d /etc/nixos 2775 root nerv-staff -"
-    "A /etc/nixos - - - - g:nerv-staff:rwx,d:g:nerv-staff:rwx" # x on FILES is not great but nya can't see a way to do it without `setfacl` so the alternative is prob a systemd oneshot
+    "d /etc/nixos/ 2775 root nerv-staff -"
+    "d /etc/nixos/bethany-base 2775 snuppy users -"
+    "d /etc/nixos/terminal-dogma 2775 root nerv-staff -"
+    "A /etc/nixos/terminal-dogma - - - - g:nerv-staff:rwx,d:g:nerv-staff:rwx,o::r" # x on ALL FILES is not great but nya can't see a way to do it without `setfacl` so the alternative is prob a systemd oneshot
   ];
+
+  systemd.user.services.setPermsOn = {
+    enable = true;
+    after = ["network.target"];
+    wantedBy = ["default.target"];
+    description = "My Cool User Service";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''/my/cool/user/service'';
+    };
+  };
 
   programs.nh = {
     enable = true;
