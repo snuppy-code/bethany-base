0ee030f27ecd45f1f6ddb79582092a97f367a5d2
snuppy <snuppy.code+git@pm.me>
2026-08-02 22:52:53 +0200

    git don't get pissed

--- modules/git-systemwide.nix
+++ modules/git-systemwide.nix
@@ -7,6 +7,7 @@
   programs.git = {
     enable = true;
     config = {
+      safe.directory = "/etc/nixos/terminal-dogma";
       init.defaultBranch = "main";
       user.name = "snuppy";
       user.email = "snuppy.code@pm.me";
