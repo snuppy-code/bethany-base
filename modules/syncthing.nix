3c4a9e1dd7b794d74e283d93e3cc32c7cc493e1a
snuppy <snuppy.code+git@pm.me>
2026-08-03 23:07:15 +0200

    fix syncthing perm issues AAARGHH
    A hundred years in the making, hold on princess, our moment has arrived

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -2,21 +2,106 @@
   config,
   pkgs,
   inputs,
+  lib,
   ...
 }: {
-  services.syncthing = {
-    enable = true;
-    openDefaultPorts = false;
+  users.groups.petrova.gid = 2019; # for folders synced by syncthing (the files seen from a far as a petrova line of migrating astrophage !)
+
+  users.users.petrova = {
+    isSystemUser = true;
+    description = "user for syncthing";
+    uid = 2019;
+    group = "petrova";
   };
-  networking.firewall.allowedTCPPorts = [
-    22000 # syncthing
-  ];
-  networking.firewall.allowedUDPPorts = [
-    22000 # syncthing
-    21027 # syncthing
+
+  sops.secrets.syncthing-password = {
+    owner = "petrova";
+    group = "petrova";
+    mode = "0400";
+  };
+
+  systemd.tmpfiles.rules = [
+    "d /home/snuppy/Documents/ryland 2770 snuppy petrova - -"
+    "d /home/snuppy/astrophage-vial 2770 snuppy petrova - -"
   ];
 
-  environment.systemPackages = with pkgs; [
-    syncthing # gui opener (?)
-  ];
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "petrova";
+    group = "petrova";
+    configDir = "/var/lib/syncthing";
+    dataDir = "/var/lib/syncthing";
+    guiAddress = "100.73.209.34:8384";
+    overrideDevices = true;
+    overrideFolders = true;
+    guiPasswordFile = config.sops.secrets.syncthing-password.path;
+    settings = {
+      gui = {
+        user = "petrova";
+      };
+      devices = {
+        "lilin" = lib.mkIf (config.networking.hostName != "lilin") {
+          autoAcceptFolders = false;
+          id = "DSN4CJM-5BYN5A5-TRVYT34-OVKZRMR-AXVYMCI-OXEUS4D-Q7C5E7A-B356OA7";
+          name = "lilin";
+        };
+        "tabris" = lib.mkIf (config.networking.hostName != "tabris") {
+          autoAcceptFolders = false;
+          id = "FGMRMX3-P5XAGRD-J6UNZ63-ESNAMMD-6RZBRA3-EWPVTPR-SC3TYCF-GKJ5TA3";
+          name = "tabris";
+        };
+        "eligius" = {
+          autoAcceptFolders = false;
+          id = "66UOSXT-RPU6FFZ-KJ7FN76-567D3B6-AJ2FXU3-7HKV46J-FW555KZ-M6XXRQ5";
+          name = "eligius";
+        };
+        "nakara" = {
+          autoAcceptFolders = false;
+          id = "MJEMDQ4-P7234HZ-QKQ4GBV-GNMYEUR-FTXLORT-EGGVELB-TAEIYCV-JX2BRAG";
+          name = "nakara";
+        };
+      };
+      folders = {
+        "ryland" = {
+          # label = "Cognition Assessment";
+          id = "zme6w-vsdht";
+          path = "/home/snuppy/Documents/ryland";
+          devices = [
+            "lilin"
+          ];
+          type = "sendreceive";
+        };
+        "astrophage-vial" = {
+          # label = "Come watch the petrovascope :D";
+          id = "udavy-lwfcd";
+          path = "/home/snuppy/astrophage-vial";
+          # remove tabris or lilin (?)?
+          devices = lib.lists.remove config.networking.hostName [
+            "lilin"
+            "tabris"
+            "eligius"
+            "nakara"
+          ];
+
+          type = "sendreceive";
+        };
+      };
+      options.urAccepted = 1;
+    };
+  };
+  # https://www.freedesktop.org/software/systemd/man/latest/systemd.exec.html
+  systemd.services.syncthing = {
+    after = ["systemd-tmpfiles-setup.service"]; # urrgh stupid stupid game
+    serviceConfig = {
+      ProtectHome = "tmpfs";
+      BindPaths = [
+        "/home/snuppy/Documents/ryland"
+        "/home/snuppy/astrophage-vial"
+      ];
+      ProtectSystem = "strict";
+      ReadWritePaths = ["/var/lib/syncthing"];
+      UMask = "0002";
+    };
+  };
 }
--- modules/users.nix
+++ modules/users.nix
@@ -12,6 +12,7 @@
     isNormalUser = true;
     extraGroups = [
       "nerv-staff"
+      "petrova"
       "wheel"
       "networkmanager"
       "adbusers"
@@ -19,7 +20,7 @@
       "dialout"
       "cdrom"
     ];
-    # uid = 2004;
+    # uid = 2004; # todo: uncomment this line,, and hope it doesn't fuck everything up
     hashedPasswordFile = config.sops.secrets.snuppy-password.path;
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIKULoTLRUxXh/H32tYRncHD4KGxXZC2lUryf0X5w6QMPAAAABHNzaDo= snuppy.code@pm.me"
