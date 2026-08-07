709187598fb8b692f22208c5854da8365bcaadc5
snuppy <snuppy.code+git@pm.me>
2026-08-07 15:59:53 +0200

    move module list from flake.nix to hosts

---
+++ hosts/tabris/default.nix
@@ -0,0 +1,53 @@
+{
+  config,
+  inputs,
+  ...
+}: {
+  imports = [
+    ./modules/hardware-configuration.nix
+    ./modules/os.nix
+    ./modules/gpu.nix
+    ./modules/cuda.nix
+    inputs.sops-nix.nixosModules.sops
+    inputs.nvf.nixosModules.default
+    inputs.stylix.nixosModules.stylix
+    inputs.home-manager.nixosModules.home-manager
+    inputs.nix-flatpak.nixosModules.nix-flatpak
+    ../../modules/sops.nix
+    ../../modules/base-hardware.nix
+    ../../modules/gnome.nix
+    ../../modules/virtualization.nix
+    ../../modules/regional.nix
+    ../../modules/audio.nix
+    ../../modules/security.nix
+    ../../modules/exposed.nix
+    ../../modules/ssh.nix
+    ../../modules/syncthing.nix
+    ../../modules/kde-connect.nix
+    ../../modules/git-systemwide.nix
+    ../../modules/nix.nix
+    ../../modules/users.nix
+    ../../modules/cli.nix
+    ../../modules/nvf.nix
+    ../../modules/common-apps.nix
+    ../../modules/thunar.nix
+    ../../modules/steam.nix
+    ../../modules/ld.nix
+    ../../modules/flatpak.nix
+    ../../modules/fonts.nix
+    ../../modules/stylix.nix
+    ../../modules/home-manager.nix
+    {
+      home-manager.users.snuppy = {
+        imports = [
+          ./modules-hm/snuppy.nix
+          inputs.sops-nix.homeManagerModule
+          ../../modules-hm/home-manager.nix
+          ../../modules-hm/gnome.nix
+          ../../modules-hm/stylix.nix
+          ../../modules-hm/virtualization.nix
+        ];
+      };
+    }
+  ];
+}
