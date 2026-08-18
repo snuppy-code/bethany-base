82452e79546bf6235fd7fff625b7535a0d2ecec3
snuppy <snuppy.code+git@pm.me>
2026-08-18 14:50:51 +0200

    kde-connect

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -17,7 +17,6 @@
   environment.systemPackages = with pkgs.gnomeExtensions; [
     tiling-shell
     emoji-copy
-    gsconnect
     caffeine
     clipboard-indicator
     appindicator
--- modules/kde-connect.nix
+++ modules/kde-connect.nix
@@ -4,17 +4,8 @@
   inputs,
   ...
 }: {
-  # https://userbase.kde.org/KDEConnect#:~:text=can%27t%20see%20each%20other
-  networking.firewall.allowedTCPPortRanges = [
-    {
-      from = 1714;
-      to = 1764;
-    }
-  ];
-  networking.firewall.allowedUDPPortRanges = [
-    {
-      from = 1714;
-      to = 1764;
-    }
-  ];
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
+  };
 }
