0e3ec19b911ebc875e5c6f6e4fe7781bef155890
snuppy <snuppy.code@pm.me>
2026-09-17 23:14:10 +0200

    ANOTHER stow (niri)

--- stow/fish/.config/fish/functions/sto.fish
+++ stow/fish/.config/fish/functions/sto.fish
@@ -1,3 +1,3 @@
-function sto --wraps='stow -d /etc/nixos/bethany-base/stow -t ~ -S zed fish ssh kitty lazygit starship espanso' --description 'alias sto stow -d /etc/nixos/bethany-base/stow -t ~ -S zed fish ssh kitty lazygit starship espanso'
-    stow -d /etc/nixos/bethany-base/stow -t ~ -S zed fish ssh kitty lazygit starship espanso $argv
+function sto
+    stow -d /etc/nixos/bethany-base/stow -t ~ -S zed fish ssh kitty lazygit starship espanso compose niri $argv
 end
